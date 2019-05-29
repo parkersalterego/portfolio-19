446c9830f6f5e5cf8797dda0656718b22911f53a
parkersalterego <parkersalterego92@gmail.com>
2019-05-29 11:14:38 -0700

    router added and about and contact components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.scss';
+
+// components
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const App = () => (
   <div className="App">
-    App
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Switch>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/contact' component={Contact}/>
+      </Switch>
+    </Router>
   </div>
 );
 
